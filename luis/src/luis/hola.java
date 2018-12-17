7db01121249aa1e21fb9499b4f1cc401cc1dfe3a
Luis Guardia <g.luisalberto68@gmail.com>
2018-12-17 05:16:37 -0400

    hola este es una prueba

---
+++ luis/src/luis/hola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luis;
+import java.io.*;
+/**
+ *
+ * @author CHELSEA
+ */
+public class hola {
+    int x=10;
+    public hola(){
+        mostrar(x);
+    }public void mostrar(int y){
+        System.out.println(y);
+    }
+}
